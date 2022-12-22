8ebea02f5e2bc969e47c2d3bd6a92039e4f0220e
ANBUclc0012 <ninhictu2001@gmail.com>
2022-12-22 14:20:58 +0700

    ninh

--- nodejs.js
+++ nodejs.js
@@ -1,11 +1,10 @@
-const { createServer } = require("http");
-const { Server } = require("socket.io");
-
-const httpServer = createServer();
-const io = new Server(httpServer, { /* options */ });
+const httpServer = require("http").createServer();
+const io = require("socket.io")(httpServer, {
+    // ...
+});
 
 io.on("connection", (socket) => {
-    console.log("a user connected");
+    console.log("New client connected");
 });
 
 httpServer.listen(3000);
